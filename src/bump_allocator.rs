7a3e0d1c86af6e439b9809c6ecd285436ba28167
Oleksandr Babak <alexanderbabak@proton.me>
2024-03-01 10:17:47 +0100

    feat!: change initialization of allocator

--- src/bump_allocator.rs
+++ src/bump_allocator.rs
@@ -1,27 +1,49 @@
-use core::{cell::RefCell, mem};
+use core::{cell::RefCell, mem, ptr};
+
+#[repr(C)]
+union SliceOrEmpty {
+    #[allow(dead_code)]
+    slice: &'static mut [u8],
+    empty: [usize; 2],
+}
 
 pub struct BumpAllocator {
-    buffer: RefCell<&'static mut [u8]>,
+    buffer: RefCell<SliceOrEmpty>,
 }
 
 impl BumpAllocator {
-    pub fn new(buffer: &'static mut [u8]) -> Self {
-        let buffer = RefCell::new(buffer);
+    #[must_use]
+    pub const fn new() -> Self {
+        // Non-zero pointer
+        let buffer = RefCell::new(SliceOrEmpty { empty: [1, 0] });
         Self { buffer }
     }
 
+    pub fn replace_buffer(&self, buffer: &'static mut [u8]) -> &'static mut [u8] {
+        let mut borrow_mut = self.get_refcell().borrow_mut();
+        mem::replace(&mut *borrow_mut, buffer)
+    }
+
     pub fn alloc<T>(&self) -> &'static mut T {
-        let size = core::mem::size_of::<T>();
-        let align = core::mem::align_of::<T>();
+        let size = mem::size_of::<T>();
+        let align = mem::align_of::<T>();
 
         let allocated = self.alloc_inner(size, align);
-        let ptr = core::ptr::from_mut::<[u8]>(allocated).cast::<T>();
+        let ptr = ptr::from_mut::<[u8]>(allocated).cast::<T>();
 
+        // SAFETY: allocated is properly aligned and has the correct size,
+        //         memory is uniquely referenced
         unsafe { &mut *ptr }
     }
 
+    fn get_refcell(&self) -> &RefCell<&'static mut [u8]> {
+        // Transmute from `[usize; 2]` to `&mut [u8]`
+        // array's first element is always non zero
+        unsafe { &*ptr::from_ref(&self.buffer).cast() }
+    }
+
     fn alloc_inner(&self, size: usize, align: usize) -> &'static mut [u8] {
-        let mut borrow_mut = self.buffer.borrow_mut();
+        let mut borrow_mut = self.get_refcell().borrow_mut();
         let current_address = borrow_mut.as_ptr() as usize;
         let to_cut = ((current_address + align - 1) & !(align - 1)) - current_address;
 
@@ -48,7 +70,8 @@ mod test {
     fn test_bump_allocator() {
         static BUFFER: StaticCell<[u8; 100]> = StaticCell::new();
         let buffer = BUFFER.init([0; 100]);
-        let allocator = BumpAllocator::new(buffer);
+        let allocator = BumpAllocator::new();
+        assert!(allocator.replace_buffer(buffer).is_empty());
         let a = allocator.alloc::<u32>();
         let b = allocator.alloc::<u8>();
         let c = allocator.alloc::<u32>();
@@ -64,7 +87,8 @@ mod test {
     fn test_bump_allocator_with_bad_align() {
         static BUFFER: StaticCell<[u8; 100]> = StaticCell::new();
         let buffer = BUFFER.init([0; 100]);
-        let allocator = BumpAllocator::new(&mut buffer[1..]);
+        let allocator = BumpAllocator::new();
+        assert!(allocator.replace_buffer(&mut buffer[1..]).is_empty());
         let a = allocator.alloc::<u32>();
         let b = allocator.alloc::<u8>();
         let c = allocator.alloc::<u32>();
@@ -81,7 +105,8 @@ mod test {
     fn test_bump_allocator_out_of_memory() {
         static BUFFER: StaticCell<[u8; 100]> = StaticCell::new();
         let buffer = BUFFER.init([0; 100]);
-        let allocator = BumpAllocator::new(buffer);
+        let allocator = BumpAllocator::new();
+        assert!(allocator.replace_buffer(buffer).is_empty());
         let a = allocator.alloc::<u32>();
         let b = allocator.alloc::<[u8; 100]>();
         core::hint::black_box(&a);
