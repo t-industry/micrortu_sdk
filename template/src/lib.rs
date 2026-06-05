58ba33298519800bcace9640579a9665223cc90a
Oleksandr Babak <alexanderbabak@proton.me>
2026-06-05 16:09:22 +0200

    fix(template): it was totally broken

--- template/src/lib.rs
+++ template/src/lib.rs
@@ -1,34 +1,45 @@
-use micrortu_sdk::{BlockPorts, FactoryInput, Shared, StepResult, params, ports, register_block};
-use static_cell::StaticCell;
+mod counter {
+    use micrortu_sdk::{
+        BlockPorts, FactoryInput, Shared, StepResult, params, ports, register_block,
+    };
+    use static_cell::StaticCell;
 
-pub struct Counter;
+    pub struct Counter;
 
-ports! {
-    #[block_names(counter)]
-    pub struct Ports {
-      count: TI13 InOut 1 1,
+    ports! {
+        #[block_names(counter)]
+        pub struct Ports {
+          count: TI13 InOut 1 1,
+        }
     }
+    params! {
+        #[block_names(counter)]
+        pub struct Params {}
+    }
+
+    pub fn factory(_: &FactoryInput) -> Option<&'static mut Counter> {
+        static COUTNER: StaticCell<Counter> = StaticCell::new();
+        Some(COUTNER.init(Counter))
+    }
+
+    pub fn init(_: &mut Shared, _: &mut Counter) -> StepResult {
+        0
+    }
+
+    pub fn step(shared: &mut Shared, _: &mut Counter) -> StepResult {
+        let ports = Ports::parse(&mut shared.latched_ports[..]);
+
+        ports.count.value += 1.;
+
+        0
+    }
+
+    register_block!(Counter, counter, factory, init, step);
 }
-params! {
-    #[block_names(counter)]
-    pub struct Params {}
+
+#[unsafe(no_mangle)]
+pub extern "C" fn init() {
+    micrortu_sdk::init_logger();
 }
 
-pub fn factory(_: &FactoryInput) -> Option<&'static mut Counter> {
-    static COUTNER: StaticCell<Counter> = StaticCell::new();
-    Some(COUTNER.init(Counter))
-}
-
-pub fn init(_: &mut Shared, _: &mut Counter) -> StepResult {
-    0
-}
-
-pub fn step(shared: &mut Shared, _: &mut Counter) -> StepResult {
-    let ports = Ports::parse(&mut shared.latched_ports[..]);
-
-    ports.count.value += 1.;
-
-    0
-}
-
-register_block!(Counter, counter, factory, init, step);
+micrortu_sdk::finalize!();
